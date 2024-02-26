679d0a2dfad8c13290cbfa416213dfc7f30ca850
Anaxímeno Brito <anaximenobrito@gmail.com>
2024-02-26 20:22:07 -0100

    feat: Add zero one norm and clusterization

--- multispectral_analysis.py
+++ multispectral_analysis.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+from sklearn.cluster import KMeans
+from multispectral_image import MultispectralRGNImage, IMAGE_HEIGHT, IMAGE_WIDTH
+
 
 def window_mean(input_image: np.ndarray, window_shape: tuple[int, int]) -> float:
     assert window_shape[0] < input_image.shape[0] and window_shape[1] < input_image.shape[1]
@@ -11,3 +14,20 @@ def window_mean(input_image: np.ndarray, window_shape: tuple[int, int]) -> float
             window = input_image[v_step:v_lim, h_step:h_lim]
             output_image[v_step:v_lim, h_step:h_lim] = window.mean()
     return output_image
+
+
+def zero_one_norm(arr: np.ndarray) -> np.ndarray:
+    return (arr - arr.min()) / (arr.max() - arr.min())
+
+
+def clusterize_multispectral_image(model: KMeans, mimage: MultispectralRGNImage) -> np.ndarray:
+    ndvi_image_band = zero_one_norm(mimage.ndvi().image)
+    gndvi_image_band = zero_one_norm(mimage.gndvi().image)
+    nir_image_band = zero_one_norm(mimage.nir_band)
+
+    flat_data = np.empty((IMAGE_HEIGHT * IMAGE_WIDTH, 3))
+
+    for i, band in enumerate((ndvi_image_band, gndvi_image_band, nir_image_band)):
+        flat_data[:, i-1] = band.flatten()
+
+    return model.predict(flat_data)
